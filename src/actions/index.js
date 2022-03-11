88164b4f27e854db521255635a986bce4b6429eb
PBottino <pbottino@axxonconsulting.com>
2022-03-11 10:02:22 -0300

    redux with hooks

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,18 @@
-import {SET_POKEMON} from './type'
-export const setPokemon = (payload) => ({
-    type: SET_POKEMON,
-    payload
-})
+import { SET_POKEMONS, SET_ERROR, CLEAR_ERROR } from './type';
+
+
+export const setPokemons = (payload) => ({
+  type: SET_POKEMONS,
+  payload,
+});
+
+export const setError = (payload) => ({
+  type: SET_ERROR,
+  payload,
+});
+
+export const clearError = (payload) => ({
+  type: CLEAR_ERROR,
+  payload,
+});
+
